f64bdbc19dcd5a428b24c7583479cdf6df40f65e
adam <ashell@gmail.com>
2013-02-24 23:53:41 -0500

    More screwing around, little moving box things.

--- src/net/granseal/scalaGDX/Assets.scala
+++ src/net/granseal/scalaGDX/Assets.scala
@@ -4,8 +4,10 @@ import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.Gdx
+import scala.util.Random
 
 object Assets {
 	val box = new Texture("assets/box.png")
 	val font = new BitmapFont(Gdx.files.internal("assets/thefont.fnt"),Gdx.files.internal("assets/thefont.png"),false)
+	val rnd = new Random()
 }
--- src/net/granseal/scalaGDX/Game.scala
+++ src/net/granseal/scalaGDX/Game.scala
@@ -9,18 +9,20 @@ import com.badlogic.gdx.graphics.GL10
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.graphics.FPSLogger
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
 
 class Game extends ApplicationListener {
   
   var x: Float = _
   var y: Float = _
   var sb: SpriteBatch = _
-  var box: Sprite = _
-  var blt: Sprite = _
   var cam: OrthographicCamera = _
   var totalTime: Double = 0
   var lastTimeDraw = 0
-  val fps = new FPSLogger()
+  var fps = new FPSLogger()
+  val entities = new ArrayBuffer[Entity]
+
   
   def create(): Unit = {
     
@@ -28,25 +30,26 @@ class Game extends ApplicationListener {
     sb = new SpriteBatch()
     cam = new OrthographicCamera()
     cam.setToOrtho(false,800,600)
-    box = new Sprite(Assets.box)
-    box.setScale(2)
-    
-    blt = new Sprite(Assets.box)
-    blt.setScale(0.2f)
+
+    for (i <- 1 to 100)  {
+        val x = Assets.rnd.nextFloat*Gdx.graphics.getWidth()
+        val y = Assets.rnd.nextFloat*Gdx.graphics.getHeight()
+    	entities += new Entity(x,y,new Sprite(Assets.box))
+    }
    
   }
   def dispose(): Unit = {}
   def pause(): Unit = {}
   def render(): Unit = {
-      Gdx.gl.glClearColor(0, 0, 0, 1);
+      Gdx.gl.glClearColor(0.2f, 0.5f, 0.5f, 1);
       Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
     sb.begin()
     	Assets.font.setColor(0,1,1,1)
     	Assets.font.draw(sb, "Hello World", 30, 30)
-    	box.draw(sb)
-    	blt.draw(sb)
+    	entities.foreach(_.draw(sb))
     sb.end()
     update(Gdx.graphics.getDeltaTime())
+    entities.foreach(_.update(Gdx.graphics.getDeltaTime()))
     fps.log()
   }
   def resize(x: Int, y: Int): Unit = {}
@@ -56,8 +59,7 @@ class Game extends ApplicationListener {
     totalTime += delta
     x = Gdx.input.getX().toFloat
     y = -Gdx.input.getY().toFloat + 600
-    box.setPosition(x, y)
-    blt.setPosition(x, y+100)
+
     if (totalTime.toInt != lastTimeDraw){
       lastTimeDraw = totalTime.toInt
       System.out.println("Time: " + totalTime.toInt)
