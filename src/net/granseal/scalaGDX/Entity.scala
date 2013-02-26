9efdbd806b7a4b77b9b8dfdcc31152f22e8d304d
RogueShadow <ashell@gmail.com>
2013-02-25 20:47:24 -0500

    Edumacational. Drawing shapes instead of a plain box image.
    Signed-off-by: RogueShadow <ashell@gmail.com>

--- src/net/granseal/scalaGDX/Entity.scala
+++ src/net/granseal/scalaGDX/Entity.scala
@@ -3,11 +3,13 @@ package net.granseal.scalaGDX
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.math.Circle
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 
 
-class Entity(var x: Float, var y: Float, var sprite: Sprite) {
+class Entity(var x: Float, var y: Float) {
   
-  var speed = 5f*Assets.rnd.nextFloat+1
+  var speed = 7f*Assets.rnd.nextFloat+1
   var direction = Assets.rnd.nextFloat*Math.PI.toFloat*2;
   var rotation = direction
   val spin = if (Assets.rnd.nextBoolean) -1 else 1
@@ -15,23 +17,21 @@ class Entity(var x: Float, var y: Float, var sprite: Sprite) {
   def update(delta: Float) = {
     x = x + (speed*Math.cos((direction)).toFloat)*delta
     y = y + (speed*Math.sin((direction)).toFloat)*delta
-    sprite
-    speed = speed + 0.01f*Assets.rnd.nextFloat
-    direction += 0.001f*Assets.rnd.nextFloat*spin
-    rotation = direction
+
+    speed = speed + 0.03f*Assets.rnd.nextFloat
+    direction += 0.0005f*Assets.rnd.nextFloat*spin
+    rotation = direction.toDegrees.toFloat
   }
   
-  def draw(sb: SpriteBatch) = {
-    sprite.setPosition(x, y)
-    sprite.setRotation(Math.toDegrees(rotation).toFloat)
-    sprite.draw(sb)
-    val head = new Sprite(sprite)
-    head.setScale(0.2f)
-    head.setColor(Color.MAGENTA)
-    val hx = x + Math.cos(direction).toFloat*20
-    val hy = y + Math.sin(direction).toFloat*20
-    head.setPosition(hx, hy)
-    head.draw(sb)
+  def draw(sr: ShapeRenderer) = {
+
+    sr.begin(ShapeRenderer.ShapeType.FilledTriangle)
+      sr.identity()
+      sr.translate(x, y, 0)
+      sr.rotate(0, 0, 1, rotation-90)
+      sr.filledTriangle(0,+32,-32,-32,+32,-32)
+    sr.end()
+
   }
   
 }
--- src/net/granseal/scalaGDX/Game.scala
+++ src/net/granseal/scalaGDX/Game.scala
@@ -1,22 +1,22 @@
 package net.granseal.scalaGDX
 
+import scala.collection.mutable.ArrayBuffer
+
 import com.badlogic.gdx.ApplicationListener
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.graphics.g2d.Sprite
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.FPSLogger
 import com.badlogic.gdx.graphics.GL10
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.InputProcessor
-import com.badlogic.gdx.graphics.FPSLogger
-import scala.collection.mutable.ArrayBuffer
-import scala.util.Random
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+
 
 class Game extends ApplicationListener {
   
   var x: Float = _
   var y: Float = _
   var sb: SpriteBatch = _
+  var shapeRender: ShapeRenderer = _
   var cam: OrthographicCamera = _
   var totalTime: Double = 0
   var lastTimeDraw = 0
@@ -27,43 +27,56 @@ class Game extends ApplicationListener {
   def create(): Unit = {
     
     Gdx.graphics.setVSync(false)
-    sb = new SpriteBatch()
-    cam = new OrthographicCamera()
+    
+    sb = new SpriteBatch
+    shapeRender = new ShapeRenderer
+    
+    cam = new OrthographicCamera
     cam.setToOrtho(false,800,600)
 
     for (i <- 1 to 100)  {
-        val x = Assets.rnd.nextFloat*Gdx.graphics.getWidth()
-        val y = Assets.rnd.nextFloat*Gdx.graphics.getHeight()
-    	entities += new Entity(x,y,new Sprite(Assets.box))
+        val x = Assets.rnd.nextFloat*Gdx.graphics.getWidth
+        val y = Assets.rnd.nextFloat*Gdx.graphics.getHeight
+    	entities += new Entity(x,y)
     }
    
   }
   def dispose(): Unit = {}
   def pause(): Unit = {}
   def render(): Unit = {
-      Gdx.gl.glClearColor(0.2f, 0.5f, 0.5f, 1);
-      Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+    
+    update(Gdx.graphics.getDeltaTime())
+    entities.foreach(_.update(Gdx.graphics.getDeltaTime()))
+    
+    cam.update()
+    shapeRender.setProjectionMatrix(cam.combined)
+    
+    Gdx.gl.glClearColor(0.2f, 0.5f, 0.5f, 1);
+    Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+    
+    entities.foreach(_.draw(shapeRender))
+    
     sb.begin()
-        entities.foreach(_.draw(sb))
     	Assets.font.setColor(0,1,1,1)
     	Assets.font.draw(sb, "Time: " + totalTime.toInt, 30, 30)
     sb.end()
-    update(Gdx.graphics.getDeltaTime())
-    entities.foreach(_.update(Gdx.graphics.getDeltaTime()))
+    
     fps.log()
   }
   def resize(x: Int, y: Int): Unit = {}
   def resume(): Unit = {}
 
   def update(delta: Float): Unit = {
+    
+    cam.zoom += 0.0002f
+    cam.rotate(0.05f)
     totalTime += delta
+    
     x = Gdx.input.getX().toFloat
     y = -Gdx.input.getY().toFloat + 600
 
-    if (totalTime.toInt != lastTimeDraw){
+    if (totalTime.toInt != lastTimeDraw)
       lastTimeDraw = totalTime.toInt
-      System.out.println("Time: " + totalTime.toInt)
-    }
     
   }
   
