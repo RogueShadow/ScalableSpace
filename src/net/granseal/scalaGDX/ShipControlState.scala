0ddcfd59e1b158ca1afe7ed721afd8635a82eef0
RogueShadow <ashell@gmail.com>
2013-03-25 22:40:32 -0400

    reset() to control state. So you don't have to make a new one to reset it, or set it all by hand again. Dunno, maybe it isn't actually a bother to make a new one?

--- src/net/granseal/scalaGDX/ShipControlState.scala
+++ src/net/granseal/scalaGDX/ShipControlState.scala
@@ -6,4 +6,12 @@ class ShipControlState(val shipID: Int) {
   var Right_Thruster = false
   var Space_Brake = false
   var PrimaryWeaponActive = false
+  
+  def reset(){
+    Main_Thruster = false
+    Left_Thruster = false
+    Right_Thruster = false
+    Space_Brake = false
+    PrimaryWeaponActive = false
+  }
 }
